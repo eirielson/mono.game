12cd9671da725542833d0afe96aca245899136f2
Eirielson Rodrigues <eirielson@gmail.com>
2015-11-14 10:32:24 -0200

    Criada interface de Sprite com implementacao default

---
+++ lab.mono.game/ISprite.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace lab.mono.game
+{
+    public interface ISprite
+    {
+        Vector2 Posicao { get; set; }
+
+        float Rotacao { get; set; }
+
+        bool OrigemCentralizada { get; set; }
+
+        float Escala { get; set; }
+
+        void LoadContent(ContentManager content);
+
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
---
+++ lab.mono.game/SpriteDefault.cs
@@ -0,0 +1,39 @@
+﻿using lab.mono.game;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+public class SpriteDefault : ISprite
+{
+    private readonly string nomeTextura;
+    private Texture2D textura;
+    private Vector2 origem;
+
+    public float Escala { get; set; }
+
+    public bool OrigemCentralizada { get; set; }
+
+    public Vector2 Posicao { get; set; }
+
+    public float Rotacao { get; set; }
+
+
+    public SpriteDefault(string nomeTextura)
+    {
+        this.nomeTextura = nomeTextura;
+    }
+
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(textura, Posicao, null, Color.White, Rotacao, origem, Escala, SpriteEffects.None, 0);
+    }
+
+    public void LoadContent(ContentManager content)
+    {
+        textura = content.Load<Texture2D>(nomeTextura);
+
+        if (OrigemCentralizada)
+            origem = new Vector2(textura.Width, textura.Height) / 2f;
+    }
+}
